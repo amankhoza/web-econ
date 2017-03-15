7bfbffce15037f371e28c98a2e4c1252f17457ad
Aman Khosa <amankhoza@hotmail.co.uk>
2017-03-15 15:04:39 +0000

    Add progress bar

--- constBidPrice.py
+++ constBidPrice.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import sys
 import os
 import time
+
+
+def progress(count, total, status='Complete'):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', status))
+    if count==total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
 
 os.system('reset')
 
@@ -12,11 +25,21 @@ df = pd.read_csv('train.csv')
 
 advertisers = list(set(df['advertiser']))
 
-n = len(df)
+lowerBidLimit = 200
+upperBidLimit = 400
+bidIncrement = 1
+
+n = len(advertisers) * len(range(lowerBidLimit, upperBidLimit, bidIncrement))
+i = 0
+j = 1
+
+optimalConstantBidPrices = []
 
 for advertiser in advertisers:
     highest = (advertiser, 0, 0)
-    for bid in range(200, 400):
+    for bid in range(lowerBidLimit, upperBidLimit, bidIncrement):
+        i += 1
+        progress(i, n, '('+str(j)+'/'+str(len(advertisers))+')')
         rows = df[(df['advertiser']==advertiser) & (df['bidprice']<=bid)]
         imps = len(rows)  # imps = number of impressions
         clicks = len(rows[rows['click']==1])
@@ -26,7 +49,11 @@ for advertiser in advertisers:
             ctr = 0
         if ctr > highest[2]:
             highest = (advertiser, bid, ctr)
-    print(highest)
+    optimalConstantBidPrices.append(highest)
+    j += 1
+
+for optimalConstantBid in optimalConstantBidPrices:
+    print(optimalConstantBid)
 
 end = time.time()
 print('\nTime elapsed: {} minutes {} seconds'.format(int((end-start)/60), int((end-start) % 60)))
