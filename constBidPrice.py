47bbe46d60fd2f2bad65b88e0fde4874f754f90e
Aman Khosa <amankhoza@hotmail.co.uk>
2017-03-26 21:00:05 +0100

    Clean code and evaluate results

--- constBidPrice.py
+++ constBidPrice.py
@@ -1,61 +1,76 @@
+from __future__ import division  # for float division instead of int division
 import pandas as pd
-import sys
-import os
 import time
+from helperFunctions import evaluateBiddingStrategy as evaluate
+from helperFunctions import progress as progress
+from helperFunctions import clearTerminal as clearTerminal
+from helperFunctions import printElapsedTime as printElapsedTime
 
 
-def progress(count, total, status='Complete'):
-    bar_len = 60
-    filled_len = int(round(bar_len * count / float(total)))
-    percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', status))
-    if count==total:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
+def train(training_set, lowerBidLimit=200, upperBidLimit=400, bidIncrement=1):
+    print('Training constant bidding strategy model:')
+    trainingDF = pd.read_csv(training_set)
+
+    advertisers = list(set(trainingDF['advertiser']))
+
+    n = len(advertisers) * len(range(lowerBidLimit, upperBidLimit, bidIncrement))
+    i = 0
+    j = 1
+
+    optimalConstBidPrices = []
+    optimalConstBids = {}
+
+    for advertiser in advertisers:
+        highest = (advertiser, 0, 0)  # tuple containing highest ctr and corresponding bid prices
+        for bid in range(lowerBidLimit, upperBidLimit, bidIncrement):
+            i += 1
+            progress(i, n, '('+str(j)+'/'+str(len(advertisers))+')')
+            rows = trainingDF[(trainingDF['advertiser']==advertiser) & (trainingDF['bidprice']<=bid)]
+            imps = len(rows)  # imps = number of impressions
+            clicks = len(rows[rows['click']==1])
+            if imps > 0:
+                ctr = clicks / imps  # ctr = click through rate
+            else:
+                ctr = 0
+            if ctr > highest[1]:
+                highest = (advertiser, ctr, bid)
+        optimalConstBidPrices.append(highest)
+        optimalConstBids[advertiser] = highest[2]
+        j += 1
+
+    for optimalConstBidPrice in optimalConstBidPrices:
+        print(optimalConstBidPrice)
+
+    return optimalConstBids
 
 
-os.system('reset')
+def predict(constant_bidprices, validation_set):
+    print('Predicting bid prices using constant bidding strategy model:')
+    bids = {}
+    validationDF = pd.read_csv(validation_set)
+
+    n = len(validationDF)
+
+    for i in range(0, n):
+        progress(i+1, n)
+        bidid = validationDF.bidid.values[i]
+        advertiser = validationDF.advertiser.values[i]
+        constBidPrice = constant_bidprices[advertiser]
+        bids[bidid] = constBidPrice
+
+    return bids
+
+
+clearTerminal()
 
 start = time.time()
-
 print('Start')
 
-df = pd.read_csv('train.csv')
-
-advertisers = list(set(df['advertiser']))
-
-lowerBidLimit = 200
-upperBidLimit = 400
-bidIncrement = 1
-
-n = len(advertisers) * len(range(lowerBidLimit, upperBidLimit, bidIncrement))
-i = 0
-j = 1
-
-optimalConstantBidPrices = []
-
-for advertiser in advertisers:
-    highest = (advertiser, 0, 0)
-    for bid in range(lowerBidLimit, upperBidLimit, bidIncrement):
-        i += 1
-        progress(i, n, '('+str(j)+'/'+str(len(advertisers))+')')
-        rows = df[(df['advertiser']==advertiser) & (df['bidprice']<=bid)]
-        imps = len(rows)  # imps = number of impressions
-        clicks = len(rows[rows['click']==1])
-        if imps > 0:
-            ctr = float(clicks) / float(imps)  # ctr = click through rate
-        else:
-            ctr = 0
-        if ctr > highest[2]:
-            highest = (advertiser, bid, ctr)
-    optimalConstantBidPrices.append(highest)
-    j += 1
-
-for optimalConstantBid in optimalConstantBidPrices:
-    print(optimalConstantBid)
+const_bidprices = train('train.csv')
+bids = predict(const_bidprices, 'validation.csv')
+evaluate(bids, 'validation.csv', 25000)
 
 end = time.time()
-print('\nTime elapsed: {} minutes {} seconds'.format(int((end-start)/60), int((end-start) % 60)))
+printElapsedTime(start, end)
 
 print('Finish')
