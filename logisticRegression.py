462e2a15ce5957b9a52ed62387de8952441abf78
Aman Khosa <amankhoza@hotmail.co.uk>
2017-04-08 20:40:37 +0100

    Work out pCTR (taking too long to run)

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,13 +1,16 @@
+from __future__ import division
+
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
 from helperFunctions import clearTerminal
+from sklearn import svm
 
 
 ALL_FEATURES = ['weekday', 'hour', 'region', 'city', 'slotwidth', 'slotheight', 'slotprice']
 CONTINOUS_FEATURES = ['slotwidth', 'slotheight', 'slotprice']
-CATEGORICAL_FEATURES = ['weekday', 'hour', 'region', 'city']
+CATEGORICAL_FEATURES = ['weekday', 'hour', 'region', 'city'] 
 LABEL = 'click'
 
 
@@ -30,20 +33,48 @@ def getX(data_set):
 clearTerminal()
 
 # Load datasets
-training_set = pd.read_csv("train.csv")
+training_set = pd.read_csv("validation.csv")
 validation_set = pd.read_csv("validation.csv")
 
 x_train = getX(training_set)
 x_valid = getX(validation_set)
 y_train = training_set[LABEL].values.tolist()
 y_valid = validation_set[LABEL].values.tolist()
+# y_train = [1] * len(training_set)
+# y_train[0] = 0
+# y_valid = [1] * len(validation_set)
+# y_valid[0] = 0
 
 logregressor = LogisticRegression()
 logregressor.fit(x_train, y_train)
 y_pred = logregressor.predict(x_valid)
 
 print('xdim', x_train.ndim)
-print(len(y_pred), len(y_valid))
-print(type(y_pred), type(y_valid))
-print(sum(y_pred), sum(y_valid))
-print(y_pred)
+progba = logregressor.predict_proba(x_train)
+progba2 = map(lambda x: x[1], progba)
+print('sams sum', sum(progba2))
+print(x_train)
+print(x_train.shape)
+# print(progba)
+# print('hello')
+# print(progba2)
+# print(len(y_pred), len(y_valid))
+# print(type(y_pred), type(y_valid))
+# print(sum(y_pred), sum(y_valid))
+# print(y_pred)
+# print(logregressor.score(x_train, y_train))
+sum1 = 0
+sum0 = 0
+c1 = 0
+c0 = 0
+for i in range(0, len(y_pred)):
+    if y_valid[i] == 1.0:
+        # print(i, y_pred[i], y_valid[i], progba[i])
+        sum1 += progba2[i]
+        c1 += 1
+    else:
+        sum0 += progba2[i]
+        c0 += 1
+
+print(sum1, c1, sum1/c1)
+print(sum0, c0, sum0/c0)
