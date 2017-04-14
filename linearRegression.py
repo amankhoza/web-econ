6d47bd3f4cf944388020b89b006968a31e0f91b9
Aman Khosa <amankhoza@hotmail.co.uk>
2017-04-14 16:30:37 +0100

    Add solutions csv output

--- linearRegression.py
+++ linearRegression.py
@@ -18,8 +18,8 @@ from helperFunctions import clampScaleFactors
 '''
 Features chosen via LinearSVC see featureSelection.py
 '''
-CONTINOUS_FEATURES = ['slotprice', 'slotheight', 'slotwidth']
-CATEGORICAL_FEATURES = ['advertiser', 'weekday', 'region', 'useragent', 'adexchange', 'slotvisibility']
+CONTINOUS_FEATURES = ['slotheight', 'slotwidth']
+CATEGORICAL_FEATURES = ['advertiser', 'weekday', 'region', 'useragent', 'adexchange']
 
 
 def getX(data_set):
@@ -66,7 +66,13 @@ start = time.time()
 print('Start')
 
 training_set = pd.read_csv('train.csv')
-validation_set = pd.read_csv('validation.csv')
+
+SOLUTION = True  # set this to true to generate solutions csv
+
+if SOLUTION:
+    validation_set = pd.read_csv('test.csv')
+else:
+    validation_set = pd.read_csv('validation.csv')
 
 print('Loaded data')
 
@@ -80,8 +86,9 @@ bidid_valid = validation_set['bidid'].values.tolist()
 x_train = getX(training_set)
 x_valid = getX(validation_set)
 bidprice_train = training_set['bidprice'].values.tolist()
-bidprice_valid = validation_set['bidprice'].values.tolist()
-click_valid = validation_set['click'].values.tolist()
+if not SOLUTION:
+    bidprice_valid = validation_set['bidprice'].values.tolist()
+    click_valid = validation_set['click'].values.tolist()
 click_train = training_set['click'].values.tolist()
 
 print('Split into train,valid sets (one hot encoding complete)')
@@ -96,7 +103,8 @@ logistic_pctrs = logisticRegressionPctrs(x_train, click_train, x_valid)
 
 print('Made logistic predictions')
 
-avgctr = avgCtr(validation_set)
+# avgctr = avgCtr(validation_set)
+avgctr = avgCtr(training_set)
 
 linear_scale_factors = scalePctrs(linear_pctrs, avgctr)
 logistic_scale_factors = scalePctrs(logistic_pctrs, avgctr)
@@ -107,12 +115,13 @@ logistic_scale_factors = clampScaleFactors(logistic_scale_factors)
 clicked_linear_scale_factors = []
 clicked_logistic_scale_factors = []
 
-print('comparison of linear/logistic scale factors')
-for i in range(0, len(click_valid)):
-    if click_valid[i] == 1:
-        print(i, linear_scale_factors[i], logistic_scale_factors[i])
-        clicked_linear_scale_factors.append(linear_scale_factors[i])
-        clicked_logistic_scale_factors.append(logistic_scale_factors[i])
+if not SOLUTION:
+    print('comparison of linear/logistic scale factors')
+    for i in range(0, len(click_valid)):
+        if click_valid[i] == 1:
+            print(i, linear_scale_factors[i], logistic_scale_factors[i])
+            clicked_linear_scale_factors.append(linear_scale_factors[i])
+            clicked_logistic_scale_factors.append(logistic_scale_factors[i])
 
 linear_scaled_bidprices = scaleBids(bidprice_pred, linear_scale_factors)
 logistic_scaled_bidprices = scaleBids(bidprice_pred, logistic_scale_factors)
@@ -121,9 +130,17 @@ unscaled_bids = dict(zip(bidid_valid, bidprice_pred))
 linear_scaled_bids = dict(zip(bidid_valid, linear_scaled_bidprices))
 logistic_scaled_bids = dict(zip(bidid_valid, logistic_scaled_bidprices))
 
-evaluate(unscaled_bids, 'validation.csv', 6250)
-evaluate(logistic_scaled_bids, 'validation.csv', 6250)
-evaluate(linear_scaled_bids, 'validation.csv', 6250)
+if SOLUTION:
+    out = open('testing_bidding_price.csv', 'w')
+    out.write('bidid,bidprice\n')
+    for i in range(0, len(linear_scaled_bidprices)):
+        out.write(str(bidid_valid[i])+','+str(linear_scaled_bidprices[i])+'\n')
+    out.flush()
+    out.close()
+else:
+    evaluate(unscaled_bids, 'validation.csv', 6250)
+    evaluate(logistic_scaled_bids, 'validation.csv', 6250)
+    evaluate(linear_scaled_bids, 'validation.csv', 6250)
 
 end = time.time()
 printElapsedTime(start, end)
